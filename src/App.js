8b24b36595676413743bf9cf8ea0d7227bf6d801
eric <eric355402@yahoo.com>
2020-06-22 19:47:46 -0500

    added login component to start testing okta on the front-end

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 const Home = lazy(() => import("./components/Home"));
 const Navbar = lazy(() => import("./components/Navbar"));
+const Login = lazy(() => import("./components/Login"));
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/Login" component={Login} />
           </Switch>
         </Suspense>
       </Router>
