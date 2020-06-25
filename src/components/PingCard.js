c9162667a42926c835832a6dce7b8193d8507b73
eric <eric355402@yahoo.com>
2020-06-24 22:02:49 -0600

    changed pingCard to feed style

--- src/components/PingCard.js
+++ src/components/PingCard.js
@@ -1,81 +1,47 @@
 import React from "react";
-import { Button, Card, Image } from "semantic-ui-react";
+import { Card, Feed } from "semantic-ui-react";
 
 const PingCard = () => (
-  <Card.Group>
-    <Card>
-      <Card.Content>
-        <Image
-          floated="right"
-          size="mini"
-          src="/images/avatar/large/steve.jpg"
-        />
-        <Card.Header>Steve Sanders</Card.Header>
-        <Card.Meta>Friends of Elliot</Card.Meta>
-        <Card.Description>
-          Steve wants to add you to the group <strong>best friends</strong>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className="ui two buttons">
-          <Button basic color="green">
-            Approve
-          </Button>
-          <Button basic color="red">
-            Decline
-          </Button>
-        </div>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image
-          floated="right"
-          size="mini"
-          src="/images/avatar/large/molly.png"
-        />
-        <Card.Header>Molly Thomas</Card.Header>
-        <Card.Meta>New User</Card.Meta>
-        <Card.Description>
-          Molly wants to add you to the group <strong>musicians</strong>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className="ui two buttons">
-          <Button basic color="green">
-            Approve
-          </Button>
-          <Button basic color="red">
-            Decline
-          </Button>
-        </div>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image
-          floated="right"
-          size="mini"
-          src="/images/avatar/large/jenny.jpg"
-        />
-        <Card.Header>Jenny Lawrence</Card.Header>
-        <Card.Meta>New User</Card.Meta>
-        <Card.Description>
-          Jenny requested permission to view your contact details
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className="ui two buttons">
-          <Button basic color="green">
-            Approve
-          </Button>
-          <Button basic color="red">
-            Decline
-          </Button>
-        </div>
-      </Card.Content>
-    </Card>
-  </Card.Group>
+  <Card>
+    <Card.Content>
+      <Card.Header>Recent Activity</Card.Header>
+    </Card.Content>
+    <Card.Content>
+      <Feed>
+        <Feed.Event>
+          <Feed.Label image="/images/ping7.png" />
+          <Feed.Content>
+            <Feed.Date content="1 day ago" />
+            <Feed.Summary>
+              You added <a href="www.google.com">Jenny Hess</a> to your{" "}
+              <a href="www.google.com">coworker</a> group.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+
+        <Feed.Event>
+          <Feed.Label image="/images/ping7.png" />
+          <Feed.Content>
+            <Feed.Date content="3 days ago" />
+            <Feed.Summary>
+              You added <a href="www.google.com">Molly Malone</a> as a friend.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+
+        <Feed.Event>
+          <Feed.Label image="/images/ping7.png" />
+          <Feed.Content>
+            <Feed.Date content="4 days ago" />
+            <Feed.Summary>
+              You added <a href="www.google.com">Elliot Baker</a> to your{" "}
+              <a href="www.google.com">musicians</a> group.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+      </Feed>
+    </Card.Content>
+  </Card>
 );
 
 export default PingCard;
