f5691386f701c4aef725372d15fdbc633b2c1b17
Aaron Andersen <aaronandersen@zohomail.com>
2020-07-15 23:31:04 -0500

    Fixed the Styling for the NavBar.

--- src/AppDrawer.jsx
+++ src/AppDrawer.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import clsx from 'clsx';
 import { useOktaAuth } from "@okta/okta-react";
-import {fade,makeStyles, useTheme } from '@material-ui/core/styles';
+import {fade, makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -25,8 +24,13 @@ import EventCard from "./components/EventCard";
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
+   root: {
+     display: 'flex',
+   },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: { display: "block" },
   },
   appBar: {
     transition: theme.transitions.create(['margin', 'width'], {
@@ -79,12 +83,19 @@ const useStyles = makeStyles((theme) => ({
     }),
     marginLeft: 0,
   },
+  shiftRight: {
+    position: "fixed",
+    display: "flex",
+    marginRight: "2rem",
+    right:0,
+  },
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     "&:hover": { backgroundColor: fade(theme.palette.common.white, 0.25) },
     marginLeft: 0,
+    marginRight: "2rem",
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
@@ -132,38 +143,34 @@ export default function PersistentDrawerLeft() {
       <CssBaseline />
       <AppBar
         position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
+        className={clsx(classes.appBar, { [classes.appBarShift]: open})}>
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
+            className={clsx(classes.menuButton, open && classes.hide)}>
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap>
-            Ping-Catcher
-          </Typography>
-          <div className={classes.search}>
+          <img src='/images/Ping_Catcher.png' className='logo' alt='application' />
+            <div className={classes.shiftRight}>
+            <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
             </div>
             <InputBase
               placeholder="Search…"
-              classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              inputProps={{ "aria-label": "search" }}
-            />
-          </div>
+              classes={{root: classes.inputRoot,input: classes.inputInput}}
+              inputProps={{ 'aria-label': 'search' }}/>
+            </div>
           {!authState.isPending && authState.isAuthenticated && (
-            <Button color="inherit" as="a" onClick={logout}>
+            <Button color="inherit" as="a" onClick={logout}
+             classes={{}}>
               Logout
             </Button>
           )}
+            </div>
         </Toolbar>
       </AppBar>
       <Drawer
@@ -196,11 +203,7 @@ export default function PersistentDrawerLeft() {
           ))}
         </List>
       </Drawer>
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open,
-        })}
-      >
+      <main className={clsx(classes.content, { [classes.contentShift]: open})}>
         <div className={classes.drawerHeader} />
             <EventCard/>
       </main>
