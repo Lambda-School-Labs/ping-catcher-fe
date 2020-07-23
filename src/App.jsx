96b9325a167ed8ebcc71648bd94934739cf55271
Trevor Martin <trevor.j.martin@gmail.com>
2020-07-23 17:57:03 -0400

    fix landingpage flashbacks with material-ui Progress indicator, phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { SecureRoute, LoginCallback } from "@okta/okta-react";
 import LandingPage from "./components/landingPage/page/LandingPage";
 import ResponsiveDrawer from "./AppDrawerNew";
 import Profile from "./Profile";
+import { LinearProgress } from "@material-ui/core";
 import "./App.css";
 
 function App() {
@@ -12,21 +13,38 @@ function App() {
   const login = async () => authService.login("/");
   const logout = async () => authService.logout("/");
 
+  const showLandingPage = !authState.isPending && !authState.isAuthenticated;
+  const showSpinner = authState.isPending;
+  const showDashboard = !authState.isPending && authState.isAuthenticated;
+
+  const conditionalRender = () => {
+    if (showSpinner) {
+      // TODO: replace/modify/theme this progress indicator.
+      return (
+        <>
+          ping-catcher
+          <h1>
+            <LinearProgress />
+          </h1>
+        </>
+      );
+    }
+    if (showDashboard) {
+      return <ResponsiveDrawer />;
+    }
+    if (!showSpinner && !showDashboard && showLandingPage) {
+      return (
+        <LandingPage login={login} logout={logout} authState={authState} />
+      );
+    }
+    return <h1>ERROR : conditional render encountered unknown condition.</h1>;
+  };
   return (
     <>
-      {authState.isAuthenticated ? (
-        <>
-          <ResponsiveDrawer />
-        </>
-      ) : (
-        <>
-          <LandingPage login={login} logout={logout} authState={authState} />
-        </>
-      )}
-      <SecureRoute path="/profile" component={Profile} />
+      {conditionalRender()}
       <Route path="/implicit/callback" component={LoginCallback} />
+      <SecureRoute path="/profile" component={Profile} />
     </>
   );
 }
-
 export default App;
