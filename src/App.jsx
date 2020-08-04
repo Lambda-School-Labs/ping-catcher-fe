be9d5eaf9bcbb9d2ddcc4ff68eeb25ecd744ff8b
eric <eric355402@yahoo.com>
2020-08-03 19:40:23 -0600

    added ranking form route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React, { useState, useMemo } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import { useOktaAuth } from '@okta/okta-react'
-import { LoginCallback } from '@okta/okta-react'
-import LandingPage from './components/landingPage/page/LandingPage'
+import React, { useState, useMemo } from "react";
+import { Route, Switch } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { LoginCallback } from "@okta/okta-react";
+import LandingPage from "./components/landingPage/page/LandingPage";
 // ResponsiveDrawer component moved to DashboardPage
-import DashPage from './components/dashboard/dashboardPage/DashboardPage'
-import { LinearProgress, ThemeProvider, Switch as MuiSwitch, Paper, createMuiTheme, } from '@material-ui/core'
+import DashPage from "./components/dashboard/dashboardPage/DashboardPage";
+import RankingForm from "./Old_Components/RankingForm.js";
+import {
+  LinearProgress,
+  ThemeProvider,
+  Switch as MuiSwitch,
+  Paper,
+  createMuiTheme,
+} from "@material-ui/core";
 
 function prefersDarkMode() {
   if (!window.matchMedia) return;
@@ -14,21 +21,21 @@ function prefersDarkMode() {
   return darkMode.matches;
 }
 
-function App () {
+function App() {
   // This is the auth logic in the top most part of the App.
   // Pass this down to both LandingPage and DashboardPage components
-  const { authState, authService } = useOktaAuth()
+  const { authState, authService } = useOktaAuth();
   // Review the Okta path on these
-  const login = async () => authService.login('/')
-  const logout = async () => authService.logout('/')
+  const login = async () => authService.login("/");
+  const logout = async () => authService.logout("/");
 
   // variables to manipulate the '/' root of App to display correct page depanding on auth state.
   // Show the spinner if waiting to get auth back
-  const showSpinner = authState.isPending
+  const showSpinner = authState.isPending;
   // Show Landing page if not authorized - no token
-  const showLandingPage = !authState.isPending && !authState.isAuthenticated
+  const showLandingPage = !authState.isPending && !authState.isAuthenticated;
   // Show the Dashboard page component
-  const showDashboard = authState.isAuthenticated
+  const showDashboard = authState.isAuthenticated;
 
   // Setting up Dark Mode to be used at the App.js level.
   const [darkMode, setDarkMode] = useState(prefersDarkMode());
@@ -42,7 +49,7 @@ function App () {
     [darkMode]
   );
 
-    // const theme = createMuiTheme({
+  // const theme = createMuiTheme({
   //   palette: {
   //     type: darkMode ? "dark" : "light",
   //   },
@@ -58,18 +65,20 @@ function App () {
             <LinearProgress />
           </h1>
         </>
-      )
+      );
     }
     if (showDashboard) {
       // change to DashboardPage component and pass authState props
-      return <DashPage logout={logout} />
+      return <DashPage logout={logout} />;
     }
     if (!showSpinner && !showDashboard && showLandingPage) {
       // LandingPage component with authState for logging in.
-      return <LandingPage login={login} logout={logout} authState={authState} />
+      return (
+        <LandingPage login={login} logout={logout} authState={authState} />
+      );
     }
-    return <h1>ERROR : conditional render encountered unknown condition.</h1>
-  }
+    return <h1>ERROR : conditional render encountered unknown condition.</h1>;
+  };
   return (
     <div style={{ width: "100%" }}>
       <ThemeProvider theme={theme}>
@@ -88,11 +97,10 @@ function App () {
             checked={darkMode}
             onChange={() => setDarkMode(!darkMode)}
           />
-          <h3 style={{position: "fixed", top: 70, left: 1200}}>
-            Toggle
-          </h3>
+          <h3 style={{ position: "fixed", top: 70, left: 1200 }}>Toggle</h3>
           <Switch>
             <Route path="/implicit/callback" component={LoginCallback} />
+            <Route path="/rank" component={RankingForm} />
           </Switch>
         </Paper>
       </ThemeProvider>
@@ -107,4 +115,4 @@ function App () {
   //   </>
   // )
 }
-export default App
+export default App;
