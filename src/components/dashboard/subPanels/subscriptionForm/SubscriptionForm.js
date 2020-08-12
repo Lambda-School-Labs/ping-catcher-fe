ba594d02f893a55156d5840984cc7105705196cc
eric <eric355402@yahoo.com>
2020-08-11 20:44:54 -0600

    set up form to push slackId with form

--- src/components/dashboard/subPanels/subscriptionForm/SubscriptionForm.js
+++ src/components/dashboard/subPanels/subscriptionForm/SubscriptionForm.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import RankingForm from "../../../../components/RankingForm.js";
 
-const SubscriptionForm = () => {
+const SubscriptionForm = ({ slackState }) => {
   return (
     <>
       <div style={{ marginLeft: "25%", marginTop: "5%" }}>
         <h1>Configure a Subscription</h1>
-        <RankingForm />
+        <RankingForm slackState={slackState} />
       </div>
     </>
   );
