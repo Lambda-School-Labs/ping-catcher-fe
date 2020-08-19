df55179773c030134f91ea3be4d95e94a4af8525
Eric Santos <Eric355402@yahoo.com>
2020-08-18 21:16:37 -0600

    created menu to be mobile responsive (#81)

--- src/components/dashboard/dashNav/DashboardNav.js
+++ src/components/dashboard/dashNav/DashboardNav.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { AppBar, Toolbar } from "@material-ui/core";
-import NavBarContent from "./dashNavComponents/NavContent";
+import NavContent from "./dashNavComponents/NavContent";
 
 // import the appBar theme from page component
-const DashNavbar = ({ style, darkMode, setDarkMode }) => {
+const DashNavbar = ({ style, darkMode, setDarkMode, handleDrawerToggle }) => {
   return (
     <AppBar className={style}>
       <Toolbar>
-        <img src="/images/Ping_Catcher.png" alt="Ping Catcher" width="9%" />
-        <NavBarContent darkMode={darkMode} setDarkMode={setDarkMode} />
+        <img
+          src="/images/Ping_Catcher.png"
+          alt="Ping Catcher"
+          width="9%"
+          onClick={handleDrawerToggle}
+        />
+        <NavContent darkMode={darkMode} setDarkMode={setDarkMode} />
       </Toolbar>
     </AppBar>
   );
