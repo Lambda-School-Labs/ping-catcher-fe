a601c87fee1f379e9ed46a8969b2364bddbd25c3
Aaron Andersen <aaronandersen@zohomail.com>
2020-08-18 21:25:52 -0500

    Changed the background color of the landing page.

--- src/components/landingPage/page/LandingPage.jsx
+++ src/components/landingPage/page/LandingPage.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import { CssBaseline, makeStyles } from "@material-ui/core";
 import Hero from "../hero/Hero";
 import SlackCard from "../infoCard/SlackCard";
 import NotifyCard from "../infoCard/NotifyCard";
 import OrganizeCard from "../infoCard/OrganizeCard";
 import Footer from "../footer/Footer";
 import "./LandingPage.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "#393e46"
+  },
+}));
+
 const LandingPage = ({ logout, login, authState }) => {
+  const classes = useStyles();
   return (
-    <>
-      <Hero login={login} logout={logout} authState={authState} />
-      <section className="info">
-        <SlackCard />
-        <NotifyCard />
-        <OrganizeCard />
-      </section>
-      <Footer title="Ping Catcher" />
-    </>
+    <div className={classes.root}>
+      <CssBaseline>
+        <Hero login={login} logout={logout} authState={authState} />
+        <section className="info">
+          <SlackCard />
+          <NotifyCard />
+          <OrganizeCard />
+        </section>
+        <Footer title="Ping Catcher" />
+      </CssBaseline>
+    </div>
   );
 };
 export default LandingPage;
