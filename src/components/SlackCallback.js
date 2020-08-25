821d4146cec621fc41d244c7e2f06c45d7215efe
Aaron Andersen <aaronandersen@zohomail.com>
2020-08-24 21:41:46 -0500

    undid everything I did tonight

--- src/components/SlackCallback.js
+++ src/components/SlackCallback.js
@@ -1,20 +1,16 @@
 import React, { useEffect } from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 const SlackCallback = (props) => {
-  // const history = useHistory();
+  const history = useHistory();
   const location = useLocation();
 
   useEffect(() => {
-    console.log(location);
     props.setSlackState(location.search);
-    // history.push("/");
-  }, []);
+    history.push("/");
+  }, [location, history, props]);
 
-  // Removed these from line 11, placing them here for backup.
-  // location, history, props
-
-  return <Link to="/profile" >Click Here to Continue... </Link>;
+  return <div></div>;
 };
 
 export default SlackCallback;
